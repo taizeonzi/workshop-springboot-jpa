ea79c3b3ef139c1314e8adbea6481f9fdc316cb4
Taize Onzi <taizeonzi@gmail.com>
2025-04-04 22:07:16 -0300

    projeto criado

---
+++ src/test/java/com/projetocursojava/projetojava/ProjetojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocursojava.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
